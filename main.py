19b6294470889fb9f5c27f47557312e6f2728873
Maryam Fadaee <maryamfadaee@gmail.com>
2024-12-11 13:28:18 -0500

    add change password endpoint

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 import models
 from database import engine
 
-from routers import auth, todos, admin
+from routers import auth, todos, admin, user
 
 
 app = FastAPI()
@@ -13,3 +13,4 @@ models.Base.metadata.create_all(bind=engine)
 app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(admin.router)
+app.include_router(user.router)
