6e4f9e5203ba2526a92700dd582f140fcf84291b
Maryam Fadaee <maryamfadaee@gmail.com>
2024-12-17 11:24:15 -0500

    test update todo endpoint

--- TodoApp/test/test_todos.py
+++ TodoApp/test/test_todos.py
@@ -73,28 +73,61 @@ def test_read_all_authenticated(test_todo):
     response = client.get("/todo/1")
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == {'title': 'Test Todo', 'description': 'This is a test todo',
-                                'complete': False, 'id': 1, 'priority': 5, 'owner_id': 1}
-    
+                               'complete': False, 'id': 1, 'priority': 5, 'owner_id': 1}
+
+
 def test_get_one_todo_authenticated_not_found(test_todo):
     response = client.get("/todo/999")
     assert response.status_code == status.HTTP_404_NOT_FOUND
     assert response.json() == {'detail': 'Todo not found.'}
-    
-    
+
+
 def test_create_todo_authenticated(test_todo):
-    request_data={
+    request_data = {
         'title': 'New Todo',
         'description': 'New Todo description',
         'priority': 5,
         'complete': False
     }
-    
+
     response = client.post('/todo/', json=request_data)
     assert response.status_code == status.HTTP_201_CREATED
-    
+
     db = TestingSessionLocal()
     model = db.query(Todos).filter(Todos.id == 2).first()
     assert model.title == request_data.get('title')
     assert model.description == request_data.get('description')
     assert model.priority == request_data.get('priority')
     assert model.complete == request_data.get('complete')
+
+
+def test_update_todo(test_todo):
+    request_data = {
+        'title': 'Updated Todo',
+        'description': 'Updated Todo description',
+        'priority': 4,
+        'complete': True
+    }
+
+    response = client.put('/todo/1', json=request_data)
+    assert response.status_code == 204
+
+    db = TestingSessionLocal()
+    model = db.query(Todos).filter(Todos.id == 1).first()
+    assert model.title == 'Updated Todo'
+    assert model.description == 'Updated Todo description'
+
+
+def test_update_todo_not_found(test_todo):
+    request_data = {
+        'title': 'Updated Todo',
+        'description': 'Updated Todo description',
+        'priority': 4,
+        'complete': True
+    }
+
+    response = client.put('/todo/99', json=request_data)
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'Todo not found.'}
+
+   
